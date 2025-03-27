d5a15149a0ccc5fc3af74241ca37ecac482b8c6b
Apar Shrestha <67278825+ares-1889@users.noreply.github.com>
2025-03-27 19:33:32 +0100

    Update volcanoes.py

--- volcanoes.py
+++ volcanoes.py
@@ -4,6 +4,7 @@ import seaborn as sns
 from sklearn.preprocessing import LabelEncoder 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.
 
 
 df = pd.read_csv('The_Volcanoes_Of_Earth.csv')
@@ -88,3 +89,6 @@ target_vector = df['Last_Eruption']
 
 X_train, X_test, y_train, y_test = train_test_split(features_vectors,target_vector,test_size = 0.2, random_state = 42)
 rf  = RandomForestClassifier(n_estimators =100,random_state = 42)
+rf.fit(X_train,y_train)
+
+y_pred = rf.predict(X_test)
